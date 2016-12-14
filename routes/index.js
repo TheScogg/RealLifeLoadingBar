3dd62bc63d955c014756e5dee99308275050b8e5
James Scoggins <thescogg@gmail.com>
2016-12-13 19:06:18 -0500

    Added express-generator template

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/main', function(req, res, next) {
+  res.sendFile("index.html", {"root": "public/views"});
+});
+
+module.exports = router;
